e4c8b98c9bda843e4fc8c2fd36451a9afa1cc696
Lucas <lucasgabriellujan@hotmail.com>
2023-05-03 16:22:25 -0300

    tercer commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+let numeroRandom=Math.floor(Math.random()*10000+1);
+console.log(numeroRandom);
+let numeroRandomHtml=document.querySelector("#numeroRandom");
+numeroRandomHtml.innerHTML=numeroRandom;
+let button=document.querySelector("#boton");
+button.addEventListener("click",verificacion);
+function verificacion(evento){ 
+    evento.preventDefault();
+    let formulario=document.querySelector("#formulario");
+    let formData=new FormData(formulario);
+    let nombre=formData.get("nombre");
+    console.log(nombre); 
+    let apellido=formData.get("apellido");
+    let email=formData.get("email");
+    let jugadorPreferido=formData.get("jugadorPreferido");
+    let inputCliente=formData.get("inputCliente");
+    let verificar=document.querySelector("#verificar");
+    if (Number(inputCliente)===numeroRandom) {
+        verificar.innerHTML= "el captcha es correcto enviado";
+        formulario.reset();
+        
+    } else {
+        verificar.innerHTML="el captcha es incorrecto";
+        
+    }
+
+}

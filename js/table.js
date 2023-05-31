cbc28706ba37812bbd7601b717f41a50da2a8581
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2023-05-31 17:56:05 -0300

    lista dinamica

---
+++ js/table.js
@@ -0,0 +1,41 @@
+'user strict'
+let listaevento= []
+document.querySelector('#btn-agregar').addEventListener('click', ()=>{
+    listaevento.push({
+        evento : document.querySelector('#input-evento').value,
+        dia : document.querySelector('#input-dia').value,
+        lugar : document.querySelector('#input-lugar').value
+    })
+    mostrar()
+})
+
+function mostrar(){
+    document.querySelector('#cont-table').innerHTML= ''
+    const contTabla = document.querySelector('#cont-table')
+    const tabla= document.createElement('table')
+    const encabezado = document.createElement('tr')
+    for(let i in listaevento[0]){
+        const celda= document.createElement('th')
+        celda.textContent = i
+        encabezado.appendChild(celda)
+    }
+    tabla.appendChild(encabezado)
+
+    listaevento.forEach((evento)=>{
+        const fila= document.createElement('tr')
+        for(let pos in evento ){
+            const celda=document.createElement('td')
+            celda.textContent = evento[pos]
+            fila.appendChild(celda)
+        }
+
+        tabla.appendChild(fila)
+
+        contTabla.appendChild(tabla)
+    })
+}
+
+document.querySelector('#btn-delete').addEventListener('click', ()=>{
+    listaevento.pop()
+    mostrar()
+})

d07f4ac22cd85b5c05bf5ac1b44b59ff8f8a2f3b
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2023-06-09 20:42:20 -0300

    casi armada la lista dinamica

--- js/table.js
+++ js/table.js
@@ -1,14 +1,13 @@
 'user strict'
 let listaevento= []
-document.querySelector('#btn-agregar').addEventListener('click', agregar())
+document.querySelector('#btn-agregar').addEventListener('click', agregar)
 
 function agregar(){
     listaevento.push({
-        evento : document.querySelector('#input-evento').value,
+        nombre : document.querySelector('#input-evento').value,
         dia : document.querySelector('#input-dia').value,
         lugar : document.querySelector('#input-lugar').value
     })
-    console.log('puto')
     mostrar()
 }
 
@@ -19,45 +18,19 @@ document.querySelector('#btn-agregar3').addEventListener('click', ()=>{
 })
 
 function mostrar(){
-    document.querySelector('#cont-table').innerHTML= ''
-    const contTabla = document.querySelector('#cont-table')
-    const tabla= document.createElement('table')
-    const encabezado = document.createElement('tr')
-    for(let i in listaevento[0]){
-        const celda= document.createElement('th')
-        celda.textContent = i
-        encabezado.appendChild(celda)
-    }
-    tabla.appendChild(encabezado)
-
-    listaevento.forEach((evento)=>{
-        const fila= document.createElement('tr')
-        for(let pos in evento ){
-            const celda=document.createElement('td')
-            celda.textContent = evento[pos]
-            fila.appendChild(celda)
+    let tabla= document.querySelector('#cont-table')
+    const tr =document.createElement('tr')
+    for(let i=0; i<listaevento.length;i++){
+        tr.innerHTML= 
+            `<td>${listaevento[i].nombre}</td>
+            <td>${listaevento[i].dia}</td> 
+            <td>${listaevento[i].lugar}</td>`
+        tabla.appendChild(tr)
         }
-
-        tabla.appendChild(fila)
-
-        contTabla.appendChild(tabla)
-    })
 }
+
 
 document.querySelector('#btn-delete').addEventListener('click', ()=>{
     listaevento.pop()
     mostrar()
 })
-let medieval=[
-    {
-        "evento":"caballeros",
-        "dia":"11/06",
-        "lugar": "mar del plata",
-    },
-    {
-        "evento":"zeus",
-        "dia":"01/07",
-        "lugar": "tucuman", 
-    },
-];
-mostrar();
